182ccb4dad7299c5df61fc5da06441a550d18006
Borislav Hadzhiev <opmftw@gmail.com>
2021-05-11 19:40:39 +0300

    added infrastructure code

--- lib/cdk-starter-stack.ts
+++ lib/cdk-starter-stack.ts
@@ -1,8 +1,77 @@
+import * as autoscaling from '@aws-cdk/aws-autoscaling';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
 import * as cdk from '@aws-cdk/core';
 
 export class CdkStarterStack extends cdk.Stack {
-  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, 'vpc', {natGateways: 1});
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'alb', {
+      vpc,
+      internetFacing: true,
+    });
+
+    const listener = alb.addListener('Listener', {
+      port: 80,
+      open: true,
+    });
+
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      'sudo su',
+      'yum install -y httpd',
+      'systemctl start httpd',
+      'systemctl enable httpd',
+      'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html',
+    );
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO,
+      ),
+      machineImage: new ec2.AmazonLinuxImage({
+        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      userData,
+      minCapacity: 2,
+      maxCapacity: 3,
+    });
+
+    listener.addTargets('default-target', {
+      port: 80,
+      targets: [asg],
+      healthCheck: {
+        path: '/',
+        unhealthyThresholdCount: 2,
+        healthyThresholdCount: 5,
+        interval: cdk.Duration.seconds(30),
+      },
+    });
+
+    listener.addAction('/static', {
+      priority: 5,
+      conditions: [elbv2.ListenerCondition.pathPatterns(['/static'])],
+      action: elbv2.ListenerAction.fixedResponse(200, {
+        contentType: 'text/html',
+        messageBody: '<h1>Static ALB Response</h1>',
+      }),
+    });
+
+    asg.scaleOnRequestCount('requests-per-minute', {
+      targetRequestsPerMinute: 60,
+    });
+
+    asg.scaleOnCpuUtilization('cpu-util-scaling', {
+      targetUtilizationPercent: 75,
+    });
+
+    new cdk.CfnOutput(this, 'albDNS', {
+      value: alb.loadBalancerDnsName,
+    });
   }
 }
